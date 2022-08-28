863eebadd80663b0410184c1018793351c940499
Angelika <anzhelika48@mail.ru>
2022-08-28 13:31:52 +0500

    Решила задачи

---
+++ Zadacha_19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+
+string number = Console.ReadLine();
+
+char[] array = number.ToArray();
+
+
+if (array.Length > 5 || array.Length < 5)
+{
+    Console.WriteLine("Ошибка");
+}
+
+ if (array[0] == array[4] && array[1] == array[3])
+ {
+    Console.WriteLine("Число является палиндромом");
+ }
+ else 
+ {
+    Console.WriteLine("Число не является палиндромом");
+ }
+
+
---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index = 1;
+
+while (index <= number)
+{
+   Console.WriteLine(Math.Pow(index, 3));
+   index++;
+}
